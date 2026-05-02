a49d5e35f9c17089d36861b408e8216222561829
skulldracula02 <clementtshwarelo1@gmail.com>
2026-05-02 19:35:54 +0200

    Fixed duplicate color variables and created Theme.kt for shared colors

---
+++ app/src/main/java/com/example/hackormythapp/Theme.kt
@@ -0,0 +1,8 @@
+package com.example.hackormythapp
+
+import androidx.compose.ui.graphics.Color
+
+// APP COLOURS
+val darkOrange = Color(0xFFFF6F00)
+val lightOrange = Color(0xFFFFE0B2)
+val white = Color.White
